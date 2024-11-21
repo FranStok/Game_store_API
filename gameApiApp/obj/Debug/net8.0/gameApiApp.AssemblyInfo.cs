68adcd86df0ff1c48526ca1e6c5bcf2cedec28d5
F-Astoreca <fastoreca@sinergiasoluciones.com.ar>
2024-11-21 16:17:33 -0300

    nose x2

--- gameApiApp/obj/Debug/net8.0/gameApiApp.AssemblyInfo.cs
+++ gameApiApp/obj/Debug/net8.0/gameApiApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gameApiApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3df08bd998e69459f41a58ede4901afe925c1827")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c52fd27f281767a83fa54077a5e9792f74f2c2d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("gameApiApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gameApiApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
